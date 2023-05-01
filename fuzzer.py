d595bb30a16716ca000f469a3f2fae26f65cd040
andrewsongj <andrewsong.j@gmail.com>
2023-04-30 23:50:03 -0400

    update fuzzer output to include analysis details

--- fuzzer.py
+++ fuzzer.py
@@ -89,11 +89,11 @@ def rowHasEmptySquares(row):
             return True
     return False
 
-def findEmptySquare(position):
+def findEmptySquare(position, row_lower=0, row_upper=8):
 	foundRow = False
 	rows = position.split("/")
 	while not foundRow: #find a row with empty squares in it
-		row_num = random.randrange(0, 8) # pick the row which we change, 0-7
+		row_num = random.randrange(row_lower, row_upper) # pick the row which we change
 		if rowHasEmptySquares(rows[row_num]):
 		    break
 	row = rows[row_num]
@@ -221,21 +221,23 @@ def move_piece(position): #move a random piece somewhere
 		if piece == value:
 		    p = key
 		    break
-
-	new_r, new_col = findEmptySquare(position)
+	if p <= 1:
+		new_r, new_col = findEmptySquare(position, row_lower=1, row_upper=7)
+	else:
+		new_r, new_col = findEmptySquare(position)
 
 	removed = remove_piece(position, r=old_r, col=old_col)
 	added = add_piece(removed, r=new_r, col=new_col, piece=p)
 
 	return added
 
-def analyze():
+def analyze(position):
 	ret_stockfish = os.system("Stockfish/src/stockfish < input_sh.txt > out_stockfish.txt")
 	if ret_stockfish != 0: # the position is invalid
 		print("invalid position detected by stockfish")
 		return ["invalid"]
 
-	print("valid position")
+	print("valid position (by stockfish)")
 	
 	with open("out_stockfish.txt", "r") as out_stockfish: 
 		stockfish_analysis = out_stockfish.read().split("\n")
@@ -251,13 +253,21 @@ def analyze():
 		if stockfish_bm == '(none)': #this should indicate checkmate, which we dont care about
 			print("valid but checkmate position")
 			return ["invalid"] 
-		depth_index = bm_index-1
-		if stockfish_analysis[depth_index].split(' ')[1] != 'depth': #sanity check make sure depth is there above bestmove line
+		depth_index = bm_index-1 #index of the line that contains the last depth the engine went to
+		depth_line = stockfish_analysis[depth_index].split(' ')
+			#example depth line: "info depth 38 seldepth 10 score mate 5 time 789 nodes 1571543 nps 1991000 hashfull 20 tbhits 0 pv d1d2 e8d8 g8f6 c5f2"
+		if depth_line[1] != 'depth': #sanity check make sure depth is there above bestmove line
 			print("stockfish depth not above bestmove")
 			return ["invalid"]
-		stockfish_depth = int(stockfish_analysis[depth_index].split(' ')[2])
+		stockfish_depth = int(depth_line[2])
 		if stockfish_depth <= 3:
 			print("stockfish low depth")
+		stockfish_score = "N/A"
+		score_index = 0
+		while depth_line[score_index] != 'score' and score_index < len(depth_line):
+			score_index += 1
+		if depth_line[score_index] == 'score':
+			stockfish_score = depth_line[score_index+1] + " " + depth_line[score_index+2]
 	
 	ret_rubichess = os.system("RubiChess/src/RubiChess < input_r.txt > out_rubichess.txt")
 	if ret_rubichess != 0:
@@ -276,12 +286,19 @@ def analyze():
 			return ["invalid"]
 		rubi_bm = rubi_analysis[bm_index].split(' ')[1]
 		depth_index = bm_index-1
-		if rubi_analysis[depth_index].split(' ')[1] != 'depth': #sanity check make sure depth is there above bestmove line
+		depth_line = rubi_analysis[depth_index].split(' ')
+		if depth_line[1] != 'depth': #sanity check make sure depth is there above bestmove line
 			print("rubi depth not above bestmove")
 			return ["invalid"]
-		rubi_depth = int(rubi_analysis[depth_index].split(' ')[2])
+		rubi_depth = int(depth_line[2])
 		if rubi_depth <= 3:
 			print("rubi low depth")
+		rubi_score = "N/A"
+		score_index = 0
+		while depth_line[score_index] != 'score' and score_index < len(depth_line):
+			score_index += 1
+		if depth_line[score_index] == 'score':
+			rubi_score = depth_line[score_index+1] + " " + depth_line[score_index+2]
 
 	ret_halogen = os.system("Halogen/bin/Halogen-native.exe < input_sh.txt > out_halogen.txt")
 	if ret_halogen != 0:
@@ -300,26 +317,34 @@ def analyze():
 			return ["invalid"]
 		halogen_bm = halogen_analysis[bm_index].split(' ')[1]
 		depth_index = bm_index-1
-		if halogen_analysis[depth_index].split(' ')[1] != 'depth': #sanity check make sure depth is there above bestmove line
+		depth_line = halogen_analysis[depth_index].split(' ')
+		if depth_line[1] != 'depth': #sanity check make sure depth is there above bestmove line
 			print("halogen depth not above bestmove")
 			return ["invalid"]
-		halogen_depth = int(halogen_analysis[depth_index].split(' ')[2]) 
+		halogen_depth = int(depth_line[2]) 
 		if halogen_depth <= 3:
 			print("halogen low depth")
+		halogen_score = "N/A"
+		score_index = 0
+		while depth_line[score_index] != 'score' and score_index < len(depth_line):
+			score_index += 1
+		if depth_line[score_index] == 'score':
+			halogen_score = depth_line[score_index+1] + " " + depth_line[score_index+2]
 	to_ret = []
-	# to_ret = ["AB", "BC"]
+	# will contain objects of the form {position: position, div: AB, e1move: e2e4, e1score: , e2move: d2d4, e2score: }
 	if stockfish_bm != rubi_bm: #There is an A B divergence
-		to_ret.append("AB")
+		to_ret.append({"position":position, "div":"AB", "e1move":stockfish_bm, "e1score":stockfish_score, "e1depth":stockfish_depth, "e2move":rubi_bm, "e2score": rubi_score, "e2depth":rubi_depth})
 	if rubi_bm != halogen_bm:
-		to_ret.append("BC")
+		to_ret.append({"position":position, "div":"BC", "e1move":rubi_bm, "e1score":rubi_score, "e1depth":rubi_depth, "e2move":halogen_bm, "e2score": halogen_score, "e2depth":halogen_depth})
 	if stockfish_bm != halogen_bm: 
-		to_ret.append("AC")
+		to_ret.append({"position":position, "div":"AC", "e1move":stockfish_bm, "e1score":stockfish_score, "e1depth":stockfish_depth, "e2move":halogen_bm, "e2score": halogen_score, "e2depth":halogen_depth})
 	return to_ret
 
-def export(position_list, filename):
+def export(results, filename):
 	with open(f"{filename}.txt", "w") as f:
-		for p in position_list:
-			f.write(p +"\n")
+		for r in results:
+			f.write(str(r))
+			f.write("\n")
 
 def fuzzer(seed_position, number_required_divergences=100):
 	divergences = 0
@@ -347,27 +372,26 @@ def fuzzer(seed_position, number_required_divergences=100):
 
 		print(tentative_position)
 		with open("input_sh.txt", "w") as input_sh: #the inputs to stockfish and halogen
-			input_sh.write(f'position fen {tentative_position} w - - 0 1\ngo movetime 1000\nucinewgame')
+			input_sh.write(f'position fen {tentative_position} w - - 0 1\ngo depth 20\nucinewgame')
 		with open("input_r.txt", "w") as input_r: #the input to rubichess
-			input_r.write(f'position fen {tentative_position} w - - 0 1\ngo movetime 1000\nwait\n')
+			input_r.write(f'position fen {tentative_position} w - - 0 1\ngo depth 20\nwait\n')
 
-		result = analyze()
+		result = analyze(tentative_position)
 		if "invalid" in result:
 		    continue
 			#break
-		if "AB" in result:
-		    divergences += 1
-		    position = tentative_position 
-		    AB_divergences.append(position)
-		if "BC" in result:
-		    divergences += 1
-		    position = tentative_position 
-		    BC_divergences.append(position)
-		if "AC" in result:
-		    divergences += 1
-		    position = tentative_position 
-		    AC_divergences.append(position)
+		for r in result:
+			divergences += 1
+			position = tentative_position
+			if r["div"] == "AB":
+				AB_divergences.append(r)
+			elif r["div"] == "BC":
+				BC_divergences.append(r)
+			elif r["div"] == "AC":
+				AC_divergences.append(r)
+
 		valid_positions += 1
+		print(str(valid_positions) + " valid positions found")
 
 	print("AB Divergences: " + str(len(AB_divergences)))
 	#print(AB_divergences)
